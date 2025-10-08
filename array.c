9dbe8e94b2a6a668e05277b657c89fe039325f69
unknown <nisargwanve1@gmail.com>
2025-10-08 19:47:39 +0530

    Added new folder and multiple DSA-related HTML and C files

---
+++ array.c
@@ -0,0 +1,29 @@
+// <!-- <script>
+//     let arr=[1,3,6,8,9]
+
+
+
+//     for(let i=0;i<arr.length;i++){
+
+//          document.write( arr[i]  + "Hi")
+//         // document.write(arr[i])
+//     }
+// </script> -->
+
+#include<stdio.h>
+
+  var arr =[1,2,0,5,7]
+   int i;
+  int n;
+int main()
+{
+    for( i=0;i<=arr[i] ;i++)
+    {
+       printf("%d",arr[i]); 
+    
+    }
+    
+}
+  
+    main()
+
